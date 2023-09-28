db84fb8e9fc0970edd12b0e394b738b7bb0038aa
YanaBytkova <nedavnyaya.yana@gmail.com>
2023-09-28 22:47:44 +0300

    add search by word

--- src/js/searchByWord.js
+++ src/js/searchByWord.js
@@ -1,6 +1,6 @@
-// export const BASE_URL = "https://drinkify.b.goit.study/api/v1/cocktails/";
+
 import axios from 'axios';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const BASE_URL = "https://drinkify.b.goit.study/api/v1/cocktails/";
 const searchWord = document.querySelector('input');
@@ -8,39 +8,32 @@ const button = document.querySelector(".searching-button");
 const searchForm = document.querySelector(".search-form");
 const resultsContainer = document.querySelector(".searching-results");
 const emptySearch = document.querySelector(".empty-search");
-
+// emptySearch.style.display = 'none';
 
 const getImages = async (data) => {
-    // toggleLoader(loader, "show");
     const listImg = await makeRequest(data);
 
     if (listImg) {
         console.log("listImg", listImg.length);
       
-        if (listImg.length === 0) {
-            emptySearch.style.display = 'block';
-            return;
-        }
-    }
+        // if (listImg.length === 0) {
+        //     resultsContainer.innerHTML = '';
+        //     return;
+        // }
         renderImages(listImg);
-        //  return listImg.totalHits;
-    //     // toggleLoader(loader, "hide");
-    //     // return listImg;
-    //   }
+    }
+        
 }
 
 const searchInfoCallback = async (e) => {
   e.preventDefault();
-//   resultsContainer.innerHTML = '';
-//   loadMoreEl.classList.replace("load-more", "load-more-hidden");
-//     let page = 1;
     const formData = new FormData(e.target);
     const data = Object.fromEntries(formData);
     console.log("data", data);
     if (data.searchQuery.trim() === "") {
-        return Notify.failure("Please write the word in the field!");
+        return console.log("Please write the word in the field!");
     }
-    getImages(data);
+     getImages(data);
       e.currentTarget.reset();
   } 
 
@@ -71,9 +64,11 @@ const searchInfoCallback = async (e) => {
 
 function renderImages(images) {
    
-    const markup = images.map(({ drink, description, drinkThumb}) => `<div class="photo-card"> <h2>${drink}</h2>
-        <img src="${drinkThumb}" alt="${drink}" loading="lazy" />
-        <div class="info"><p class="info-item"> ${description}</p></div></div>`)
+    const markup = images.map(({ drink, description, drinkThumb}) => `<div class="cocktail-card"> 
+        <img src="${drinkThumb}" alt="${drink}" loading="lazy" class="cocktail-card"/>
+        <h2>${drink}</h2> 
+        <div class="info"><p class="cocktail-description"> ${description}</p></div>
+        <div class=""><button type="button" class="learn-more-button">Learn More</button> <button type="button" class="favorite"><svg class="icon" width="24px" height="24px"><use href="./images/sprite.svg#icon-favorites-tablet-desktop-white"></use></svg> </button> </div></div>`)
         .join("");
    
     resultsContainer.innerHTML = markup;
@@ -83,7 +78,6 @@ const makeRequest = async (data) => {
       
     const searchParams = new URLSearchParams({
         s: data.searchQuery,
-     
     });
     
   
@@ -97,6 +91,12 @@ const makeRequest = async (data) => {
             throw new Error(res.statusText);
         })
         // .then((res) => {
+        //     if (res.status === 404) {
+        //        emptySearch.style.display = 'block';
+        //        return console.log(emptySearch);
+        //     }
+        // })
+        // .then((res) => {
         //     loadMoreEl.classList.replace("load-more-hidden", "load-more");
         //     console.log(page * res.hits.length);
         //     if ((page * res.hits.length) >= res.totalHits) {
@@ -106,12 +106,14 @@ const makeRequest = async (data) => {
         //     return res;
         // })
         .catch((err) => {
+            
             errorMsg();
             return console.log("such cocktails not found");
         })
 }
 
 function errorMsg() {
-    emptySearch.classList.replace("empty-search-toggle", "empty-search-hidden");
-    console.log(emptySearch);
+    resultsContainer.innerHTML = emptySearch.textContent;
+    emptySearch.style.display = 'block';
+    console.log("emptySearch", emptySearch);
 }
