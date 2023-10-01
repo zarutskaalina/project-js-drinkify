dd51180bd4c6ecf9c3d8499d361e01cdb17c4823
OleksiiHaidak <xalexey.g@gmail.com>
2023-10-01 19:48:39 +0300

    update html css js files

--- src/js/createMarkupIngredients.js
+++ src/js/createMarkupIngredients.js
@@ -1,52 +1,58 @@
-const products = [
-  {
-    "_id": "64f1d5cc69d8333cf130fc22",
-    "title": "Campari",
-    "description": "A famous aperitif that is a bitter, red, herbal liqueur, well-known for its distinctive flavor and vibrant red color. Often used in cocktails.",
-    "type": "Liqueur",
-    "alcohol": "Yes",
-    "abv": "24-28.5",
-    "flavour": "Bitter and Herbal",
-    "country": "Italy"
-  },
-   {
-    "_id": "64f1d5cc69d8333cf130fc22",
-    "title": "Campari",
-    "description": "A famous aperitif that is a bitter, red, herbal liqueur, well-known for its distinctive flavor and vibrant red color. Often used in cocktails.",
-    "type": "Liqueur",
-    "alcohol": "Yes",
-    "abv": "24-28.5",
-    "flavour": "Bitter and Herbal",
-    "country": "Italy"
-    },
-    {
-    "_id": "64f1d5cc69d8333cf130fc22",
-    "title": "Campari",
-    "description": "A famous aperitif that is a bitter, red, herbal liqueur, well-known for its distinctive flavor and vibrant red color. Often used in cocktails.",
-    "type": "Liqueur",
-    "alcohol": "Yes",
-    "abv": "24-28.5",
-    "flavour": "Bitter and Herbal",
-    "country": "Italy"
-    },
-     {
-    "_id": "64f1d5cc69d8333cf130fc22",
-    "title": "Campari",
-    "description": "A famous aperitif that is a bitter, red, herbal liqueur, well-known for its distinctive flavor and vibrant red color. Often used in cocktails.",
-    "type": "Liqueur",
-    "alcohol": "Yes",
-    "abv": "24-28.5",
-    "flavour": "Bitter and Herbal",
-    "country": "Italy"
-  },
-];
+const cocktailsContainer = document.querySelector(".ingredients-cards");
+const dataPlaceholder = document.querySelector(".ingredients-placeholder");
 
 
-localStorage.setItem('products', JSON.stringify(products));
+const dataCocktails = JSON.parse(localStorage.getItem('cocktails'));
 
-const data = JSON.parse(localStorage.getItem('products'));
+let displayedCards = 6;
+
+function renderCocktails() {
+  const ingredientsCards = dataCocktails
+    .slice(0, displayedCards)
+    .map((cocktail) => `
+    <div class="ingredient-card" data-id="${cocktail._id}">
+      <h2 class="ingredient-title">${cocktail.title}</h2>
+      <p class="alcohol-level">${cocktail.alcohol === "Yes" ? "Alcoholic" : "Non-Alcoholic"}</p>
+      <p class="ingredient-description">${cocktail.description}</p>
+      <div class="ingredients-buttons">
+        <button type="button" class="learn-more-button button">learn more</button>
+        <button type="button" class="ingredients-button-remove button">
+          <svg class="icon-trash" width="18" height="18">
+            <use href="./images/sprite.svg#icon-trash-mobile-white"></use>
+          </svg>
+        </button>
+      </div>
+    </div>
+  `).join("");
+  cocktailsContainer.innerHTML = ingredientsCards;
+
+  if (dataCocktails.length > 0) {
+  dataPlaceholder.style.display = "none";
+  };
+  if (dataCocktails.length === 0) {
+  cocktailsContainer.style.display = "none";
+  };
+};
+
+renderCocktails();
 
 
-// const data = localStorage.getItem('products');
 
 
+function onDeleteCardBtn(event) {
+  const clickedElement = event.target;
+
+  if (clickedElement.classList.contains('ingredients-button-remove')) {
+    const cocktailId = clickedElement.closest('.ingredient-card').getAttribute('data-id');
+
+    const indexToDelete = dataCocktails.findIndex(cocktail => cocktail._id === cocktailId);
+
+    dataCocktails.splice(indexToDelete, 1);
+
+    localStorage.setItem('cocktails', JSON.stringify(dataCocktails));
+
+    renderCocktails();
+  }
+}
+
+cocktailsContainer.addEventListener('click', onDeleteCardBtn);
