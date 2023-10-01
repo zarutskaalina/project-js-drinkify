74567f2c3c47f942568018b1a091b76cda49c571
Oksana Zinchenko <zkosanaa@gmail.com>
2023-10-01 17:08:05 +0300

    +color-switch

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -1,44 +1,17 @@
-// const openModalBtn = document.querySelector('.header-open-modal-btn');
-// const closeModalBtn = document.querySelector('.burger-close-modal-btn');
-// const modalBackdrop = document.querySelector('.burger-backdrop');
-// const modal = document.querySelector('.burger-modal');
-
-// function openModal() {
-//     modalBackdrop.style.display = 'block';
-//     modal.style.display = 'block';
-// }
-
-
-// function closeModal() {
-//     modalBackdrop.style.display = 'none';
-//     modal.style.display = 'none';
-// }
-
-
-// function onKeyPress(event) {
-//     if (event.key === 'Escape') {
-//         closeModal();
-//     }
-// }
-
-
-// openModalBtn.addEventListener('click', openModal);
-// closeModalBtn.addEventListener('click', closeModal);
-// document.addEventListener('keydown', onKeyPress); 
 const openModalBtn = document.querySelector('.header-open-modal-btn'); 
 const closeModalBtn = document.querySelector('.burger-close-modal-btn'); 
 const modalBackdrop = document.querySelector('.burger-backdrop'); 
-const modal = document.querySelector('.burger-modal'); 
+const burgerModal = document.querySelector('.burger-modal'); 
 const favoriteLink = document.querySelector('.js-burger-list-link');
 
 function openModal() {
     modalBackdrop.style.display = 'block'; 
-    modal.style.display = 'block'; 
+    burgerModal.style.display = 'block'; 
 }
 
 function closeModal() {
     modalBackdrop.style.display = 'none'; 
-    modal.style.display = 'none'; 
+    burgerModal.style.display = 'none'; 
 }
 
 function onKeyPress(event) {
