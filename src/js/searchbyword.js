74f467f62fd08a4175a4846e572ba9d4c82f0924
YanaBytkova <nedavnyaya.yana@gmail.com>
2023-10-02 11:29:03 +0300

    add paginator function

--- src/js/searchbyword.js
+++ src/js/searchbyword.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import { createMarkupCocktail } from "./createMarkupCocktail";
-// import { paginator } from './paginator.js';
 
 const BASE_URL = "https://drinkify.b.goit.study/api/v1/cocktails/";
 const searchForm = document.querySelector(".search-form");
@@ -10,11 +9,14 @@ const searchTitle = document.querySelector(".searching-results-title");
 const randomCocktails = document.querySelector(".random-cocktails");
 const searchContainer = document.querySelector(".searching-results");
 const pagContainer = document.querySelector(".pagination-container");
+const paginationNumbers = document.querySelector('#pagination-numbers');
+const nextButton = document.querySelector('#next-button');
+const prevButton = document.querySelector('#prev-button');
+const innerWidthScreen = document.body.clientWidth;
+const paginatedList = document.querySelector('#paginated-list');
+
 emptySearch.style.display = 'none';
-
-
-
-    
+pagContainer.style.display = 'none';  
 const getImages = async (data) => {
     const listImg = await makeRequest(data);
     if (listImg) {
@@ -27,24 +29,28 @@ const getImages = async (data) => {
             randomCocktails.style.display = 'none';
             searchTitle.style.display = 'block';
         }
+        paginationNumbers.innerHTML = '';
         renderImages(listImg);
         
     }
-        
+   
 }
+   
 
 const searchInfoCallback = async (e) => {
   e.preventDefault();
+
     const formData = new FormData(e.target);
     const data = Object.fromEntries(formData);
-    console.log("data", data);
     if (data.searchQuery.trim() === "") {
         listResults.innerHTML = '';
         return console.log("Please write the word in the field!");
     }
+    paginatedList.innerHTML = '';
     getImages(data);
+   
+    e.currentTarget.reset();
     
-      e.currentTarget.reset();
   } 
 
 function renderImages(images) {
@@ -55,18 +61,17 @@ function renderImages(images) {
         .join('');
     
     listResults.insertAdjacentHTML("afterbegin", markup);
-    
+    paginator();
+   
 }
 
 
 searchForm.addEventListener("submit", searchInfoCallback);
 const makeRequest = async (data) => {
-      
     const searchParams = new URLSearchParams({
         s: data.searchQuery,
     });
     
-  
     return axios.get(`${BASE_URL}search/?${searchParams.toString()}`)
         .then((res) => {
             if (res.status === 200) {
@@ -75,23 +80,7 @@ const makeRequest = async (data) => {
         
             throw new Error(res.statusText);
         })
-        // .then((res) => {
-            
-        //        searchTitle.style.display = 'flex';
-        //        return console.log(emptySearch);
-            
-        // })
-        // .then((res) => {
-        //     loadMoreEl.classList.replace("load-more-hidden", "load-more");
-        //     console.log(page * res.hits.length);
-        //     if ((page * res.hits.length) >= res.totalHits) {
-        //         loadMoreEl.classList.replace("load-more", "load-more-hidden");
-        //         // infoMsg();
-        //     }
-        //     return res;
-        // })
         .catch((err) => {
-            
             errorMsg();
             // return console.log("such cocktails not found");
         })
@@ -99,8 +88,115 @@ const makeRequest = async (data) => {
 
 function errorMsg() {
     emptySearch.style.display = 'block';
+    pagContainer.style.display = 'none'; 
     searchTitle.style.display = 'none'
+    randomCocktails.style.display = 'none';
     listResults.innerHTML = '';
 }
 
+function paginator() {
 
+     pagContainer.style.display = 'flex'; 
+     const listItems = paginatedList.querySelectorAll("li");
+     let paginationLimit = 8;
+     if (innerWidthScreen > 1279) {
+         paginationLimit = 9; 
+     
+     } 
+     console.log(innerWidthScreen, paginationLimit);
+     console.log ("listItems",  listItems);
+ const pageCount = Math.ceil(listItems.length / paginationLimit);
+  console.log("pageCount", pageCount);
+     if (pageCount === 1) {
+         pagContainer.style.display = 'none';
+         paginationNumbers.innerHTML = '';
+ 
+     }
+  let currentPage = 1;
+  
+  const disableButton = (button) => {
+     button.classList.add("disabled");
+     button.setAttribute("disabled", true);
+   };
+   
+   const enableButton = (button) => {
+     button.classList.remove("disabled");
+     button.removeAttribute("disabled");
+   };
+   
+   const handlePageButtonsStatus = () => {
+     if (currentPage === 1) {
+       disableButton(prevButton);
+     } else {
+       enableButton(prevButton);
+     }
+   
+     if (pageCount === currentPage) {
+       disableButton(nextButton);
+     } else {
+       enableButton(nextButton);
+     }
+   };
+   const handleActivePageNumber = () => {
+     document.querySelectorAll(".pagination-number").forEach((button) => {
+       button.classList.remove("active");
+       const pageIndex = Number(button.getAttribute("page-index"));
+       if (pageIndex == currentPage) {
+         button.classList.add("active");
+       }
+     });
+   };  
+   const appendPageNumber = (index) => {
+     const pageNumber = document.createElement("button");
+     pageNumber.className = "pagination-number";
+     pageNumber.innerHTML = index;
+     console.log("index", index);
+     pageNumber.setAttribute("page-index", index);
+     pageNumber.setAttribute("aria-label", "Page " + index);
+   
+     paginationNumbers.appendChild(pageNumber);
+   };
+   const getPaginationNumbers = () => {
+     for (let i = 1; i <= pageCount; i++) {
+       appendPageNumber(i);
+     }
+   };
+   const setCurrentPage = (pageNum) => {
+     currentPage = pageNum;
+   
+     handleActivePageNumber();
+     handlePageButtonsStatus();
+     
+     const prevRange = (pageNum - 1) * paginationLimit;
+     const currRange = pageNum * paginationLimit;
+   
+     listItems.forEach((item, index) => {
+       item.classList.add("hidden");
+       if (index >= prevRange && index < currRange) {
+         item.classList.remove("hidden");
+       }
+     });
+   };
+   if (listItems.length > paginationLimit) {
+     getPaginationNumbers();
+     setCurrentPage(1);
+   
+     prevButton.addEventListener("click", () => {
+       setCurrentPage(currentPage - 1);
+     });
+   
+     nextButton.addEventListener("click", () => {
+       setCurrentPage(currentPage + 1);
+     });
+   
+     document.querySelectorAll(".pagination-number").forEach((button) => {
+       const pageIndex = Number(button.getAttribute("page-index"));
+   
+       if (pageIndex) {
+         button.addEventListener("click", () => {
+           setCurrentPage(pageIndex);
+         });
+       }
+     });
+   }
+} 
