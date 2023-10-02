177ac2c2970a586f4ac8da8d5f005920a25aa4da
OleksiiHaidak <xalexey.g@gmail.com>
2023-10-02 19:52:39 +0300

    update js and css

--- src/js/createMarkupIngredients.js
+++ src/js/createMarkupIngredients.js
@@ -1,8 +1,8 @@
-const products = [
+const cocktails = [
   {
-    "_id": "64f1d5cc69d8333cf130fc22",
-    "title": "Campari",
-    "description": "A famous aperitif that is a bitter, red, herbal liqueur, well-known for its distinctive flavor and vibrant red color. Often used in cocktails.",
+    "_id": "64f1d5cc69d8333cf1c22",
+    "title": "Campar",
+    "description": "A famous apeghjloremer, red, herbal liqueur, well-known for its distinctive flav  er, red, herbal liqueur, well-known for its distinctive flav f that is a bitter, red, herbal liqueur, well-known for its distinctive flavor and vibrant red color. Often used in cocktails.",
     "type": "Liqueur",
     "alcohol": "Yes",
     "abv": "24-28.5",
@@ -10,9 +10,79 @@ const products = [
     "country": "Italy"
   },
    {
-    "_id": "64f1d5cc69d8333cf130fc22",
-    "title": "Campari",
-    "description": "A famous aperitif that is a bitter, red, herbal liqueur, well-known for its distinctive flavor and vibrant red color. Often used in cocktails.",
+    "_id": "64f1d5c9d8333cf130fc22",
+    "title": "Campa",
+    "description": "A famous aperitif that is a bitter, red, herbal liqueur, A famousA fA famous aperitif that is a bitter, red, herbal liquamous aperitif that is a bitter, red, herbal liqu aperitif that is a bitter, red, herbal liquwell-known for its distinctive flavor and vibrant red color. Often used in cocktails.",
+    "type": "Liqueur",
+    "alcohol": "No",
+    "abv": "24-28.5",
+    "flavour": "Bitter and Herbal",
+    "country": "Italy"
+    },
+    {
+    "_id": "64f1d5cc69d833cf130fc22",
+    "title": "Capvvvv",
+    "description": "A famois a bitter, red, herbal liqA famous aperitif that is a bitter, red, herbal liqueur, well-known for its distinctive flavor and vibrant redA famous aperitif that is a bitter, red, herbal liqueur, well-known for its distinctive flavor and vibrant redueur, well-known for its distinctive flavor and vibrant red color. Often used in cocktails.",
+    "type": "Liqueur",
+    "alcohol": "Yes",
+    "abv": "24-28.5",
+    "flavour": "Bitter and Herbal",
+    "country": "Italy"
+  },
+    {
+    "_id": "64f1d5cc69d833cf130fc22",
+    "title": "Cappppp",
+    "description": "A famois a bitter, red, herbal liqA famous aperitif that is a bitter, red, herbal liqueur, well-known for its distinctive flavor and vibrant redA famous aperitif that is a bitter, red, herbal liqueur, well-known for its distinctive flavor and vibrant redueur, well-known for its distinctive flavor and vibrant red color. Often used in cocktails.",
+    "type": "Liqueur",
+    "alcohol": "Yes",
+    "abv": "24-28.5",
+    "flavour": "Bitter and Herbal",
+    "country": "Italy"
+  },
+    {
+    "_id": "64f1d5cc69d833cf130fc22",
+    "title": "Capoo",
+    "description": "A famois a bitter, red, herbal liqA famous aperitif that is a bitter, red, herbal liqueur, well-known for its distinctive flavor and vibrant redA famous aperitif that is a bitter, red, herbal liqueur, well-known for its distinctive flavor and vibrant redueur, well-known for its distinctive flavor and vibrant red color. Often used in cocktails.",
+    "type": "Liqueur",
+    "alcohol": "Yes",
+    "abv": "24-28.5",
+    "flavour": "Bitter and Herbal",
+    "country": "Italy"
+  },
+    {
+    "_id": "64f1d5cc69d833cf130fc22",
+    "title": "Capyy",
+    "description": "A famois a bitter, red, herbal liqA famous aperitif that is a bitter, red, herbal liqueur, well-known for its distinctive flavor and vibrant redA famous aperitif that is a bitter, red, herbal liqueur, well-known for its distinctive flavor and vibrant redueur, well-known for its distinctive flavor and vibrant red color. Often used in cocktails.",
+    "type": "Liqueur",
+    "alcohol": "Yes",
+    "abv": "24-28.5",
+    "flavour": "Bitter and Herbal",
+    "country": "Italy"
+  },
+    {
+    "_id": "64f1d5cc69d833cf130fc22",
+    "title": "Caip",
+    "description": "A famois a bitter, red, herbal liqA famous aperitif that is a bitter, red, herbal liqueur, well-known for its distinctive flavor and vibrant redA famous aperitif that is a bitter, red, herbal liqueur, well-known for its distinctive flavor and vibrant redueur, well-known for its distinctive flavor and vibrant red color. Often used in cocktails.",
+    "type": "Liqueur",
+    "alcohol": "Yes",
+    "abv": "24-28.5",
+    "flavour": "Bitter and Herbal",
+    "country": "Italy"
+  },
+    {
+    "_id": "64f1d5cc69d833cf130fc22",
+    "title": "Capjjjj",
+    "description": "A famois a bitter, red, herbal liqA famous aperitif that is a bitter, red, herbal liqueur, well-known for its distinctive flavor and vibrant redA famous aperitif that is a bitter, red, herbal liqueur, well-known for its distinctive flavor and vibrant redueur, well-known for its distinctive flavor and vibrant red color. Often used in cocktails.",
+    "type": "Liqueur",
+    "alcohol": "Yes",
+    "abv": "24-28.5",
+    "flavour": "Bitter and Herbal",
+    "country": "Italy"
+  },
+    {
+    "_id": "64f1d5cc69d833cf130fc22",
+    "title": "Cap",
+    "description": "A famois a bitter, red, herbal liqA famous aperitif that is a bitter, red, herbal liqueur, well-known for its distinctive flavor and vibrant redA famous aperitif that is a bitter, red, herbal liqueur, well-known for its distinctive flavor and vibrant redueur, well-known for its distinctive flavor and vibrant red color. Often used in cocktails.",
     "type": "Liqueur",
     "alcohol": "Yes",
     "abv": "24-28.5",
@@ -20,19 +90,19 @@ const products = [
     "country": "Italy"
     },
     {
-    "_id": "64f1d5cc69d8333cf130fc22",
-    "title": "Campari",
-    "description": "A famous aperitif that is a bitter, red, herbal liqueur, well-known for its distinctive flavor and vibrant red color. Often used in cocktails.",
+    "_id": "64f1d5cc69d833cf130fc22",
+    "title": "Cap",
+    "description": "A famois a bitter, red, herbal liqA famous aperitif that is a bitter, red, herbal liqueur, well-known for its distinctive flavor and vibrant redA famous aperitif that is a bitter, red, herbal liqueur, well-known for its distinctive flavor and vibrant redueur, well-known for its distinctive flavor and vibrant red color. Often used in cocktails.",
     "type": "Liqueur",
     "alcohol": "Yes",
     "abv": "24-28.5",
     "flavour": "Bitter and Herbal",
     "country": "Italy"
-    },
-     {
-    "_id": "64f1d5cc69d8333cf130fc22",
-    "title": "Campari",
-    "description": "A famous aperitif that is a bitter, red, herbal liqueur, well-known for its distinctive flavor and vibrant red color. Often used in cocktails.",
+  },
+    {
+    "_id": "64f1d5cc69d833cf130fc22",
+    "title": "Cap",
+    "description": "A famois a bitter, red, herbal liqA famous aperitif that is a bitter, red, herbal liqueur, well-known for its distinctive flavor and vibrant redA famous aperitif that is a bitter, red, herbal liqueur, well-known for its distinctive flavor and vibrant redueur, well-known for its distinctive flavor and vibrant red color. Often used in cocktails.",
     "type": "Liqueur",
     "alcohol": "Yes",
     "abv": "24-28.5",
@@ -41,12 +111,70 @@ const products = [
   },
 ];
 
-
-localStorage.setItem('products', JSON.stringify(products));
-
-const data = JSON.parse(localStorage.getItem('products'));
+localStorage.setItem('cocktails', JSON.stringify(cocktails));
 
 
-// const data = localStorage.getItem('products');
+
+import spriteUrl from '../images/sprite.svg';
+
+const cocktailsContainer = document.querySelector(".ingredients-cards");
+const dataPlaceholder = document.querySelector(".ingredients-placeholder");
+
+
+const dataCocktails = JSON.parse(localStorage.getItem('cocktails'));
+
+let displayedCards = 6;
+
+function renderCocktails() {
+  const ingredientsCards = dataCocktails
+    .slice(0, displayedCards)
+    .map((cocktail) => `
+    <div class="ingredient-card" data-id="${cocktail._id}">
+      <h2 class="ingredient-title">${cocktail.title}</h2>
+      <p class="alcohol-level">${cocktail.alcohol === "Yes" ? "Alcoholic" : "Non-Alcoholic"}</p>
+      <p class="ingredient-description">${cocktail.description}</p>
+      <div class="ingredients-buttons">
+        <button type="button" class="learn-more-button button">learn more</button>
+        <button type="button" class="ingredients-button-remove button">
+          <svg class="icon-trash" width="18" height="18">
+            <use href="${spriteUrl}#icon-trash-mobile-white"></use>
+          </svg>
+        </button>
+      </div>
+    </div>
+  `).join("");
+  cocktailsContainer.innerHTML = ingredientsCards;
+
+  if (dataCocktails.length > 0) {
+  dataPlaceholder.style.display = "none";
+  };
+  if (dataCocktails.length === 0) {
+  cocktailsContainer.style.display = "none";
+  };
+};
+
+renderCocktails();
+
+
+
+
+function onDeleteCardBtn(event) {
+  const clickedElement = event.target;
+
+  if (clickedElement.classList.contains('ingredients-button-remove')) {
+    const cocktailId = clickedElement.closest('.ingredient-card').getAttribute('data-id');
+
+    const indexToDelete = dataCocktails.findIndex(cocktail => cocktail._id === cocktailId);
+
+    dataCocktails.splice(indexToDelete, 1);
+
+    localStorage.setItem('cocktails', JSON.stringify(dataCocktails));
+
+    renderCocktails();
+  }
+}
+
+cocktailsContainer.addEventListener('click', onDeleteCardBtn);
+
 
 
